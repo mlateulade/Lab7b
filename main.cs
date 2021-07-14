e0d831619c057470fc59d8f920dafd9e27086748
mlateulade <77686194+mlateulade@users.noreply.github.com>
2021-07-14 01:52:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+
+class MainClass 
+{ 
+  public static int IntergerPower(int x, int y)
+  {    
+    int a = x;    
+
+    for(int i=0; i<y-1; i++)
+    {
+      a *=x;
+    }
+
+    return a;
+  }
+    
+
+  public static void Main (string[] args) 
+  {
+    int x;
+    int y;
+    int b;    
+
+    Console.WriteLine("Enter a base value: ");
+    x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter a powers value: ");
+    y = Convert.ToInt32(Console.ReadLine());
+      
+    b = IntergerPower(x,y);
+
+    Console.WriteLine(x + " to the power of " + y + " is " + b);
+  } 
+
+}
+
+    
